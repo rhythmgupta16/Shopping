c6cd49a29c3cbdc0a733c000863ebf0a0dced6f3
Rhythm Gupta <rhythmgupta1234@gmail.com>
2020-06-22 18:08:57 +0530

    login sign up completed

---
+++ lib/home.dart
@@ -0,0 +1,57 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title, this.uid}) : super(key: key); //update this to include the uid in the constructor
+  final String title;
+  final String uid; //include this
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  FirebaseUser currentUser;
+
+  @override
+  initState() {
+    this.getCurrentUser();
+    super.initState();
+  }
+
+  void getCurrentUser() async {
+    currentUser = await FirebaseAuth.instance.currentUser();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: <Widget>[
+          FlatButton(
+            child: Text("Log Out"),
+            textColor: Colors.white,
+            onPressed: () {
+              FirebaseAuth.instance
+                  .signOut()
+                  .then((result) =>
+                      Navigator.pushReplacementNamed(context, "/login"))
+                  .catchError((err) => print(err));
+            },
+          )
+        ],
+      ),
+      body: Center(
+        child: Container(
+            padding: const EdgeInsets.all(20.0),
+            
+             
+            )),
+      
+
+    );
+  }
+  
+}

48c443b1b3da56ab0227716011ed43fcbac3ebc8
Rhythm Gupta <rhythmgupta1234@gmail.com>
2020-06-25 06:00:13 +0530

    Login Screen UI done

--- lib/login.dart
+++ lib/login.dart
@@ -14,7 +14,6 @@ class LoginPage extends StatefulWidget {
 }
 
 
-
 class _LoginPageState extends State<LoginPage> {
   final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
   TextEditingController emailInputController;
@@ -55,41 +54,143 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Login"),
-        ),
+        
+        
         body: Container(
-            padding: const EdgeInsets.all(20.0),
+            //padding: const EdgeInsets.all(20.0),
             child: SingleChildScrollView(
-                child: Form(
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                decoration: BoxDecoration(
+                gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Color(0xffFBB034), Color(0xffF8B313)],
+                ),
+              ),
+              child: Form(
               key: _loginFormKey,
               child: Column(
                 children: <Widget>[
-                  TextFormField(
-                    decoration: InputDecoration(
-                        labelText: 'Email*', hintText: "john.doe@gmail.com"),
-                    controller: emailInputController,
-                    keyboardType: TextInputType.emailAddress,
-                    validator: emailValidator,
+                 AppBar(
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                  title: Text(
+                    "LOGIN", style: TextStyle(fontWeight: FontWeight.bold),),
+                  centerTitle: true,
+                  leading: Icon(Icons.arrow_back),
+                  // Actions are identified as buttons which are added at the right of App Bar
+                  actions: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      //child: Image.asset('assets/logo.png'),
+                      )
+                    ],
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                        labelText: 'Password*', hintText: "********"),
-                    controller: pwdInputController,
-                    obscureText: true,
-                    validator: pwdValidator,
-                  ),
-                  RaisedButton(
-                    child: Text("Login"),
-                    color: Theme.of(context).primaryColor,
-                    textColor: Colors.white,
-                    onPressed: () {
-                      if (_loginFormKey.currentState.validate()) {
-                        FirebaseAuth.instance
-                            .signInWithEmailAndPassword(
+                  ClipPath( // ClipPath is used to clip the child in a custom shape
+                    clipper: BottomClipper(),
+                    // here is the custom clipper for bottom cut shape
+                    child: Container(
+                      width: MediaQuery
+                        .of(context)
+                        .size
+                        .width,
+                      padding: EdgeInsets.only(top: 40, bottom: 30),
+                      margin: EdgeInsets.only(top: 30, left: 20, right: 20),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black,
+                              offset: Offset(1, 2),
+                              spreadRadius: 1.0,
+                              blurRadius: 5.0)
+                            ],
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(18.0),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.email),
+                            labelText: 'Email'
+                          ),
+                          controller: emailInputController,
+                          keyboardType: TextInputType.emailAddress,
+                          validator: emailValidator,
+                          ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(18.0),
+                        child: TextFormField(
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.lock),
+                              labelText: 'Password',
+                              suffixIcon: Icon(Icons.remove_red_eye),
+                            ),
+                            controller: pwdInputController,
+                            obscureText: true,
+                            validator: pwdValidator,
+                            ),
+                        ),
+                        Container(
+                        width: MediaQuery.of(context).size.width,
+                        alignment: Alignment.centerRight,
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 18.0),
+                          child: InkWell( // InkWell widget makes the widget clickable and provide call back for touch events
+                            onTap: () {
+                              print("Forget Password tap");
+                            },
+                            child: Text(
+                              'Forget Password?',
+                              textAlign: TextAlign.end,
+                              style: TextStyle(
+                                color: Color(0xffFBB034),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 20),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/phone');
+                          },
+                          child: RichText( // RichText is used to styling a particular text span in a text by grouping them in one widget
+                            text: TextSpan(
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,),
+                              text: 'Login using ',
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: 'Mobile Number and OTP',
+                                  style: TextStyle(
+                                    decoration: TextDecoration.underline,),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          InkWell(
+                            onTap: (){
+                            if (_loginFormKey.currentState.validate()) {
+                              FirebaseAuth.instance
+                              .signInWithEmailAndPassword(
                                 email: emailInputController.text,
                                 password: pwdInputController.text)
-                            .then((currentUser) => Firestore.instance
+                              .then((currentUser) => Firestore.instance
                                 .collection("users")
                                 .document(currentUser.uid)
                                 .get()
@@ -101,80 +202,152 @@ class _LoginPageState extends State<LoginPage> {
                                                   title: "Home",
                                                   uid: currentUser.uid,
                                                 ))))
-                                .catchError((err) => print(err)))
-                            .catchError((err) => print(err));
-                      }
-                    },
-                  ),
-                  Text("Don't have an account yet?"),
-                  FlatButton(
-                    child: Text("Register here!"),
-                    onPressed: () {
-                      Navigator.pushNamed(context, "/register");
-                    },
-                  ),
-                  RaisedButton(
-                    child: Text("OTP Authentication"),
-                    color: Theme.of(context).primaryColor,
-                    textColor: Colors.white,
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/phone');
-                    },
-                  ),
-                  OutlineButton(
-                    splashColor: Colors.grey,
-                    onPressed: () {
-                      signInWithGoogle().whenComplete(() {
-                        Navigator.pushNamed(context, '/home');
-                      });
-                      },
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-                    highlightElevation: 0,
-                    borderSide: BorderSide(color: Colors.grey),
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
+                              .catchError((err) => print(err)))
+                              .catchError((err) => print(err));
+                            }
+                            },
+                            child: Container(
+                              margin: EdgeInsets.only(right: 20, top: 10),
+                              decoration: BoxDecoration(
+                                  color: Color(0xffFBB034),
+                                  borderRadius: BorderRadius.circular(30)),
+                              padding: EdgeInsets.all(8),
+                              child: Icon(
+                                Icons.navigate_next,
+                                size: 40,
+                                color: Colors.white,
+                              ),
+                              ),
+                              ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      ),
+
+                 ClipPath(
+                    clipper: TopClipper(), // Custom Clipper for top clipping the social login menu box
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      padding: EdgeInsets.only(top: 50, bottom: 50),
+                      margin: EdgeInsets.only(left: 20, right: 20),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black,
+                              offset: Offset(1, 2),
+                              spreadRadius: 1.0,
+                              blurRadius: 5.0),
+                        ],
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        //Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 10),
-                          child: Text(
-                            'Sign in with Google',
-                          style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.grey,
-                        ),
-                        ),
-                        )
-                      ],
+                        children: <Widget>[
+                          Text(
+                            "Or",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Color(0xff898989),
+                            ),
+                         ),
+                          Text(
+                            "Login with Social Media",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Color(0xff898989),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: 
+                                Material(
+                                  child: InkWell(
+                                  onTap: () {
+                                     facebookLogin(context).then((user) {
+                                      if (user != null) {
+                                        print('Logged in successfully.');
+                                        Navigator.pushNamed(context, '/home');
+                                        isFacebookLoginIn = true;
+                                      } else {
+                                        print('Error while Login.');
+                                      }
+                                    });
+                                  },
+                                  child: Container(
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                      child: Image.asset('assets/fb-icon.png',
+                                        width: 80.0, height: 80.0),
+                                        ),
+                                        ),
+                                  )
+                                )
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: 
+                                Material(
+                                  child: InkWell(
+                                  onTap: () {
+                                    signInWithGoogle().whenComplete(() {
+                                    Navigator.pushNamed(context, '/home');
+                                    });
+                                  },
+                                  child: Container(
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                      child: Image.asset('assets/gmail-icon.png',
+                                        width: 80.0, height: 80.0),
+                                        ),
+                                        ),
+                                  )
+                                )
+                              ),
+                            ],
+                          )
+                        ],
                       ),
                     ),
                   ),
-
-                  RaisedButton(
-                    child: Text('Facebook Login'),
-                    color: Theme.of(context).primaryColor,
-                    textColor: Colors.white,
-                    onPressed: () {
-                      facebookLogin(context).then((user) {
-                        if (user != null) {
-                          print('Logged in successfully.');
-                          Navigator.pushNamed(context, '/home');
-                          isFacebookLoginIn = true;
-                      } else {
-                        print('Error while Login.');
-                      }
-                    });
-                  },
-                )
-              
-
+                   Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.pushNamed(context, "/register");
+                      },
+                    child: RichText(
+                    text: TextSpan(
+                      text: "Don't have an account? ",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: "Click here to signup",
+                              style:
+                            TextStyle(decoration: TextDecoration.underline),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
                 ],
               ),
-            ))));
-  }
+            ),
+        ),
+      ),
+    ),
+  );
+}
+    
+                  
+
 
 Future<FirebaseUser> facebookLogin(BuildContext context) async {
     FirebaseUser currentUser;
@@ -216,4 +389,36 @@ Future<FirebaseUser> facebookLogin(BuildContext context) async {
     return true;
   }
   
+}
+
+// Custom Clipper Class
+class BottomClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    // Add Path lines to form slight cut
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height - 50);
+    return path;
+  }
+
+  // we don't need to render it again and again as UI renders
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
+class TopClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+
+    path.lineTo(size.width, 50);
+    path.lineTo(size.width, size.height + 10);
+    path.lineTo(0, size.height + 10);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
